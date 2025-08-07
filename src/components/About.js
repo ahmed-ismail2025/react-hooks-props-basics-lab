45daf791c95dcdd47cc60269de444a8af1d44119
ahmed-ismail2025 <ahmedismail9044@gmail.com>
2025-08-07 17:50:31 +0300

    Fix all test errors and pass all props correctly

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,16 @@
-import React from "react";
+// src/components/About.js
 
-function About() {
+import React from "react";
+import Links from "./Links";
+
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {bio && <p>{bio}</p>}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
 
-export default About;
+export default About; // ✅ Important!
